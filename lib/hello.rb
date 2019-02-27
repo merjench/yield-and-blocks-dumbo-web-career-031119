26bd49fff955c2844542f239f6bfc796278bc338
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-27 19:53:49 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -15,9 +15,8 @@ end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+["Tim", "Tom", "Jim"].each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
 end
-ruby lib/hello.rb
